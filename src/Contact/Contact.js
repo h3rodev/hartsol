30c9b28ae518484a587653f0b8f8d71d68395176
h3rodev <h3rodev@users.noreply.github.com>
2018-01-17 21:34:16 +0400

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -19,7 +19,7 @@ class Contact extends Component {
       <section id="hartsol-contact" className="hartsol-view">
       	<div className="hartsol-section-container">
 	      	<h1 className="hartsol-section-title">
-	      		Contact me <br/> <a href="mailto:ronald.tolentino27@gmail.com?subject=Inquiry From Website&body=Found your website and would like your services. Please call me at +971 -- --- ----"><i className="fa fa-envelope" aria-hidden="true"></i></a><span></span> <br /> <span>+971 50 855 7027</span>
+	      		Contact me <br/> <a href="mailto:ronald.tolentino27@gmail.com?subject=Inquiry From Website&body=Found your website and would like your services. Please call me at +971 -- --- ----"><i className="fa fa-envelope" aria-hidden="true"></i></a><span></span> <span>+971 50 855 7027</span>
 	      	</h1>
 	      	<article className="content-half">
 	      		<p>If you have any questions, please don’t hesitate to contact me</p>
