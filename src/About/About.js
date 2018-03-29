d8388e5435fb9d3fe64c35878504c717ef6fba9d
h3rodev <h3rodev@users.noreply.github.com>
2018-03-29 06:05:03 +0400

    Update About.js

--- src/About/About.js
+++ src/About/About.js
@@ -11,6 +11,7 @@ class About extends Component {
 				<div className="slidingVertical">
 					<span>a web developer.</span>
 					<span>a graphic designer.</span>
+					<span>SEO enthusiast</span>
 					<span>a musician.</span>
 					<span>a songwriter.</span>
 					<span>a photographer.</span>
@@ -21,10 +22,10 @@ class About extends Component {
 				<p>Two years later I Joined <a href="#">Masterscrane LLC</a>, and I started to work as a graphic/web designer in one of the two offices.</p>
 				<p>In March 2011 I joined <a href="#">Kingsgate Real Estate</a> in Dubai, UAE as Web developer.</p>
 				<p>I can say that my skills matured more when I was hired as a Web Developer on a design agency <a href="#">BDB Advertising LLC</a> also in Dubai, UAE where in we handled big brands like Mercedes-Benz, AMG, gar gash Enterprices, Johnson’s, Chantelle, Chopard, Montegrappa, Medcare Hospital, Natixis and more..</p>
-				<p>Then I joined <a href="#">Danube Building Materials FZCO</a> as UI/Front End Developer in June 2014. I was task to develop their online shop called danubedirect.com and manage all the company websites under danube umbrella.</p>
-				<p>On December 2015 <a href="#">Leader Healthcare</a> hired me to manage their website, create landing pages for products to promote namely Hydrafacial, Zoll, Sam Medical, Heyer Medical, Morizne and more..</p>
+				<p>Then I joined <a href="#">Danube Building Materials FZCO</a> as UI/Front End Developer in June 2014. I was task to develop their online shop, make it SEO ready and manage all the company websites under danube umbrella.</p>
+				<p>On December 2015 <a href="#">Leader Healthcare</a> hired me to manage their website, do the SEO, create landing pages for products to promote namely Hydrafacial, Zoll, Sam Medical, Heyer Medical, Morizne and more..</p>
 				<p>Rest of my experience is successful cooperation as a freelancer with IT companies and professionals from Philippines and United Arab Emirates. </p>
-				<p>Right now I work as senior web developer in ( OBG ) <a href="#">One Broker Group</a> a real estate based in Dubai, UAE.</p>
+				<p>Right now I work as senior web developer in ( OBG ) <a href="#">One Broker Group</a> a real estate based in Dubai, UAE. Focused on landing pages creation, commissioned the OBG Site, implemented keywords for SEO</p>
 			</article>
 			<div className="clearfix"></div>
 	      	<a download="/pdf/cv.pdf" href="/pdf/cv.pdf" className="hartsol-btn-bordered hartsol-btn">Download CV</a>
